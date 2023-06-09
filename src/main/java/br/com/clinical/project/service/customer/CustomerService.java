e81eb89bd7b5e3b6a6e9ce52f642fd55e621738b
Diogenes <diogenes.95@hotmail.com>
2023-06-09 19:29:15 -0300

    inserindo modelmapper e retornando no dto no lugar da entidade no endpoint

--- src/main/java/br/com/clinical/project/service/customer/CustomerService.java
+++ src/main/java/br/com/clinical/project/service/customer/CustomerService.java
@@ -1,4 +1,20 @@
 package br.com.clinical.project.service.customer;
 
+import br.com.clinical.project.model.customer.Customer;
+import br.com.clinical.project.repository.customer.CustomerRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
 }
--- src/main/java/br/com/clinical/project/service/material/MaterialService.java
+++ src/main/java/br/com/clinical/project/service/material/MaterialService.java
@@ -2,6 +2,8 @@ package br.com.clinical.project.service.material;
 
 import br.com.clinical.project.model.material.Material;
 import br.com.clinical.project.repository.material.MaterialRepository;
+import br.com.clinical.project.service.material.dto.MaterialRequestDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,16 @@ public class MaterialService {
     @Autowired
     MaterialRepository materialRepository;
 
-    public Optional<Material> findById(Long materialId){
-        return materialRepository.findById(materialId);
+    @Autowired
+    ModelMapper modelMapper;
+
+    public MaterialRequestDTO findById(Long materialId){
+        Optional<Material> materialOptional = materialRepository.findById(materialId);
+        MaterialRequestDTO materialRequestDTO = new MaterialRequestDTO();
+        if(materialOptional.isPresent()){
+            Material material = materialOptional.get();
+            return materialRequestDTO.toDto(modelMapper, material);
+        }
+        return null;
     }
 }
