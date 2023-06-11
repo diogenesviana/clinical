9b86340bfacb28f0963b38f68c2c5d3e661103fe
Diogenes <diogenes.95@hotmail.com>
2023-06-10 22:30:04 -0300

    Crud basico da entidade material

--- src/main/java/br/com/clinical/project/service/material/MaterialService.java
+++ src/main/java/br/com/clinical/project/service/material/MaterialService.java
@@ -7,6 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,13 +20,38 @@ public class MaterialService {
     @Autowired
     ModelMapper modelMapper;
 
-    public MaterialRequestDTO findById(Long materialId){
-        Optional<Material> materialOptional = materialRepository.findById(materialId);
+    public MaterialRequestDTO findById(Long idMaterial){
+        Optional<Material> materialOptional = materialRepository.findById(idMaterial);
         MaterialRequestDTO materialRequestDTO = new MaterialRequestDTO();
         if(materialOptional.isPresent()){
             Material material = materialOptional.get();
             return materialRequestDTO.toDto(modelMapper, material);
         }
         return null;
+    }
+
+    public List<MaterialRequestDTO> findAll(){
+        List<MaterialRequestDTO> dtoList = new ArrayList<>();
+        MaterialRequestDTO dto = new MaterialRequestDTO();
+        List<Material> all = materialRepository.findAll();
+        for(Material material : all){
+            dtoList.add(dto.toDto(modelMapper, material));
+        }
+       return dtoList;
+    }
+
+    public void create(MaterialRequestDTO materialRequestDTO){
+        materialRepository.save(materialRequestDTO.toEntity(modelMapper, materialRequestDTO));
+    }
+
+    public MaterialRequestDTO update(Long idMaterial, MaterialRequestDTO materialRequestDTO){
+        Optional<Material> materialOptional = materialRepository.findById(idMaterial);
+        if(materialOptional.isPresent()){
+            Material material = materialOptional.get();
+            materialRequestDTO.setIdMaterial(material.getIdMaterial());
+            materialRepository.save(materialRequestDTO.toEntity(modelMapper, materialRequestDTO));
+            return materialRequestDTO;
+        }
+        throw new RuntimeException("Não encontrado!");
     }
 }
---
+++ src/main/resources/db/migration/V1_3__insert_into_material.sql
@@ -0,0 +1,2 @@
+INSERT INTO material (id_material, tx_material, qt_material) VALUES (1, 'Caneta', 20);
+INSERT INTO material (id_material, tx_material, qt_material) VALUES (2, 'Papel', 50);
