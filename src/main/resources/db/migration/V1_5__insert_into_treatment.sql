d95a44745ae2ffc49ccf871b16a191e4f9b934dd
Peterson Bersanetti <74914733+petersonbersanetti@users.noreply.github.com>
2023-06-23 17:06:03 -0300

    Inserção de Tabela de Tratamentos

---
+++ src/main/resources/db/migration/V1_5__insert_into_treatment.sql
@@ -0,0 +1,8 @@
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('1', 'RELAXING_MASSAGE', 'MASSAGEM_RELAXANTE');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('2', 'DEEP_TISSUE_MASSAGE', 'MASSAGEM_DEEP_TISSUE');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('3', 'FOOT_REFLEXOLOGY_SESSION', 'SESSÃO_DE_REFLEXOLOGIA_PODAL');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('4', 'CLINICAL_AROMATHERAPY_SESSION', 'SESSÃO_DE_AROMATERAPIA_CLINICA');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('5', 'THAI_MASSAGE_SESSION', 'SESSÃO_DE_THAI_MASSAGEM');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('6', 'ACUPUNCTURE_SESSION', 'SESSÃO_DE_ACUPUNTURA');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('7', 'BACH_FLOWER_THERAPY_CONSULTATION', 'CONSULTA_TEARAPIA_FLORAIS_DE_BACH');
+INSERT INTO `db_clinical`.`treatment` (`id_treatment`, `tx_treatment`, `tx_description`) VALUES ('8', 'INTEGRATED_NATUROLOGY_CONSULTATION ', 'CONSULTA_INTEGRADA_DE_NATUROLOGIA');
