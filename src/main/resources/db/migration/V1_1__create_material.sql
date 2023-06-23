852abf9090357ec6e474016aa006e1a1496eab89
Peterson Bersanetti <74914733+petersonbersanetti@users.noreply.github.com>
2023-06-23 16:04:10 -0300

    Solucionando problema de PRIMARY KEY

--- src/main/resources/db/migration/V1_1__create_material.sql
+++ src/main/resources/db/migration/V1_1__create_material.sql
@@ -2,4 +2,4 @@ CREATE TABLE material (
   id_material BIGINT(20) NOT NULL AUTO_INCREMENT,
   tx_material VARCHAR(100) NULL,
   qt_material BIGINT(20) NULL,
-  PRIMARY KEY (id_Material));
+  PRIMARY KEY (id_material));
--- src/main/resources/db/migration/V1_4__create_treatment.sql
+++ src/main/resources/db/migration/V1_4__create_treatment.sql
@@ -1,5 +1,5 @@
 CREATE TABLE treatment (
-  id_Treatment BIGINT(20) NOT NULL AUTO_INCREMENT,
-  tx_Treatment VARCHAR(100) NOT NULL,
-  tx_Description VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id_Treatment));
+  id_treatment BIGINT(20) NOT NULL AUTO_INCREMENT,
+  tx_treatment VARCHAR(100) NOT NULL,
+  tx_description VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id_treatment));
