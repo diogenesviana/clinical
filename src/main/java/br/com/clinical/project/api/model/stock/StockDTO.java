d8a3d6d9052d746faf07b6e9a1e857aa2aa88120
Diogenes <diogenes.95@hotmail.com>
2023-06-29 16:23:34 -0300

    feat: Ajustando dtos

--- src/main/java/br/com/clinical/project/api/model/stock/StockDTO.java
+++ src/main/java/br/com/clinical/project/api/model/stock/StockDTO.java
@@ -1,29 +1,23 @@
 package br.com.clinical.project.api.model.stock;
 
-import br.com.clinical.project.api.model.material.MaterialDTO;
-import br.com.clinical.project.api.model.material.MaterialRequestDTO;
-import br.com.clinical.project.domain.model.material.Material;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.modelmapper.ModelMapper;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class StockDTO {
 
     private Long idStock;
-    private MaterialDTO material;
+    private Long idMaterial;
     private BigDecimal qtStock;
     private LocalDateTime dtUpdated;
 
-    public StockDTO(Long idStock, MaterialDTO material, BigDecimal qtStock) {
+    public StockDTO(Long idStock, Long idMaterial, BigDecimal qtStock) {
         this.idStock = idStock;
-        this.material = material;
+        this.idMaterial = idMaterial;
         this.qtStock = qtStock;
         this.dtUpdated = LocalDateTime.now();
     }
